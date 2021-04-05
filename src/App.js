95f690e800f0b44ed454b8b31f4491427a2ba4aa
Denys Daniielian <67520699+d3n1x1@users.noreply.github.com>
2021-04-06 02:32:43 +0300

    add handlers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-function App() {
-  return <div className="App">куку</div>;
+import { Component } from 'react';
+// import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './components/ContactForm/ContactForm';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    const { contacts } = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList contacts={contacts} onDeleteContact={this.deleteContact} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+const Filter = () => {
+  return (
+    <div>
+      <label htmlFor="filter">Find contacts by name</label>
+      <input type="text" name="filter" />
+    </div>
+  );
+};
+
+export default Filter;
